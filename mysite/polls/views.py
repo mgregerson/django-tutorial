4fe58b09e5d140310890e56e2d6337bb5cf2b5aa
mgregerson <mgregerson4321@gmail.com>
2023-05-08 18:18:01 -0400

    Completed step 7, added templates for editing admin page

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -26,7 +26,7 @@ class DetailView(generic.DetailView):
 
     def get_queryset(self):
         """ Excludes any questions that aren't published yet."""
-        return Question.objects.filter(pub_date_lte = timezone.now())
+        return Question.objects.filter(pub_date__lte = timezone.now())
 
 
 class ResultsView(generic.DetailView):
